516da492d9e7789bd89f69dec56da4200e0ec68b
Manojkumar <manojbhosale@users.noreply.github.com>
2016-09-13 16:19:56 +0530

    Added iterative logic for folder printer

--- src/main/com/kumar/algos/iterationandrecursion/RecursiveDirectoryTreePrinter.java
+++ src/main/com/kumar/algos/iterationandrecursion/RecursiveDirectoryTreePrinter.java
@@ -29,5 +29,47 @@ public final class RecursiveDirectoryTreePrinter {
 			print(f,sps+SPACES);
 		}
 	}
+	
+	public void  iterativeFolderPrinter(File file){
+
+		File[] fList = file.listFiles();
+		Stack<File> st = new Stack<File>();
+		Stack<Integer> paddingSt = new Stack<Integer>();
+		String sps = "";
+		int pad = 0;
+
+		while(true){
+			for(File f : fList ){
+
+				if(f.isDirectory()){
+					//System.out.print(sps+f.getName()+"\n");
+					
+					st.add(f);
+					paddingSt.add(pad+1);
+					//System.out.println(f.getName() +" "+ (pad+1));
+				}else{
+					System.out.print(sps+f.getName()+"\n");
+				}
+			}
+			if(st.isEmpty()){
+				break;
+			}
+			pad = paddingSt.pop();
+			sps = getPadding(pad);
+			System.out.println(sps+sps.length()+st.peek().getName());
+			sps+=" ";
+			fList = st.pop().listFiles();
+		}
+
+	}
+
+	public String getPadding(int numSpaces){
+		String s= "";
+		for(int i = 0 ; i < numSpaces; i++){
+			s= s + " ";
+		}
+		return s;
+	}
+
 
 }
